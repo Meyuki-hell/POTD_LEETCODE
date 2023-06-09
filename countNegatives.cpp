4d9b5ace58f82186e7241ea33489bb0190e0fa94
MINAL TANDEKAR <110032697+Meyuki-hell@users.noreply.github.com>
2023-06-09 19:04:32 +0530

    Create countNegatives.cpp
    1351. Count Negative Numbers in a Sorted Matrix

---
+++ countNegatives.cpp
@@ -0,0 +1,16 @@
+int countNegatives(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int i=0,j=m-1;
+        int ans=0;
+        while(i<n && j>=0){
+            if(grid[i][j]<0){
+                ans+=n-i;
+                
+                j--;
+            }
+            else i++;
+
+        }
+        return ans;
+    }
